75c3bcdbbecfcb053d585bbeb11fabe7a484f7b4
Walker Chesley <chesley.walker@gmail.com>
2020-04-19 14:39:12 -0500

    added pages and logic to add products for scraping
    added page and backend logic to add products to list for scraping; saves products to database that will later be read and scraped.

---
+++ Pages/Create.cshtml.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations; 
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace WishList.Pages
+{
+    public class Create : PageModel 
+    {
+        private readonly ILogger<Create> _logger;
+        private readonly ProductContext _context;
+        [BindProperty]
+        [Required]
+        [Display(Name = "Price HTML ID:")]
+        [MinLength(1)]
+        public string priceId {get; set;}
+        [BindProperty]
+        [DataType(DataType.Url)]
+        [Required]
+        public string url {get; set;}
+        [BindProperty]
+        [Required]
+        [Display(Name = "Product Name HTML ID:")]
+        [MinLength(1)]
+        public string productNameId {get; set;}
+        public Create(ILogger<Create> logger, ProductContext context)
+        {
+            _logger = logger; 
+            _context = context; 
+        }
+
+        public void OnPost()
+        {
+            var productMeta = new ProductMeta
+            {
+                ProductUrl = url,
+                PriceHtmlId = priceId,
+                NameHtmlId = productNameId
+            };
+            try
+            {
+                _context.ProductMeta.Add(productMeta);
+                _context.SaveChanges(); 
+            }
+            catch(Exception e)
+            {
+                _logger.LogWarning(e, "Error Saving to database:"); 
+            }
+            RedirectToPage("Index"); 
+        }
+    }
+}
