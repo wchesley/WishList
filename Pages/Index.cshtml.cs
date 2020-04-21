fdc81796e7c189ddfc14afee6d02b9f4d0d0afcd
Walker Chesley <chesley.walker@gmail.com>
2020-04-21 09:19:41 -0500

    added sorting and filtering in index

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -12,7 +13,12 @@ namespace WishList.Pages
     {
         private readonly ILogger<IndexModel> _logger;
         private readonly ProductContext _context;
-        public List<Product> ProductsList {get; set;}
+        public IList<Product> ProductsList {get; set;}
+        public string NameSort {get; set;}
+        public string DateSort {get; set;}
+        public string PriceOrder {get; set;}
+        public string CurrentSort {get; set;}
+        public string searchFilter {get; set;}
 
         public IndexModel(ILogger<IndexModel> logger, ProductContext context)
         {
@@ -20,9 +26,46 @@ namespace WishList.Pages
             _context = context;
         }
 
-        public void OnGet()
+        public async Task OnGetAsync(string sortOrder, string searchString)
         {
-            ProductsList = _context.Product.ToList(); 
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name" : "name_desc";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+            PriceOrder = sortOrder == "price" ? "price_desc" : "price";
+
+            searchFilter = searchString; 
+
+            IQueryable<Product> products = from p in _context.Product select p; 
+
+            if(!String.IsNullOrEmpty(searchFilter))
+            {
+                products = products.Where(p => p.name.Contains(searchFilter)); 
+            }
+
+            switch(sortOrder)
+            {
+                case "name":
+                products = products.OrderByDescending(p => p.name);
+                break;
+                case "Date":
+                products = products.OrderBy(p => p.timeRetreived);
+                break;
+                case "date_desc":
+                products = products.OrderByDescending(p => p.timeRetreived);
+                break;
+                case "name_desc":
+                products = products.OrderBy(p => p.name);
+                break;
+                case "price":
+                products = products.OrderBy(p => p.price);
+                break;
+                case "price_desc":
+                products = products.OrderByDescending(p => p.price);
+                break;
+                default:
+                products = products.OrderBy(p => p.timeRetreived);
+                break;
+            }
+            ProductsList = await products.AsNoTracking().ToListAsync();   
         }
     }
 }
