9f1be4ed95da3f4e54350cd667b1e250c9c92d15
Walker Chesley <chesley.walker@gmail.com>
2020-04-22 15:19:19 -0500

    added pageination, searching and ordering

--- Pages/SavedProducts.cshtml.cs
+++ Pages/SavedProducts.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -12,7 +13,10 @@ namespace WishList.Pages
     {
         private readonly ProductContext _context;
         private readonly ILogger<SavedProducts> _logger;
-        public List<ProductMeta> SavedProductsList {get; set;}
+        public Paginate<ProductMeta> SavedProductsList { get; set; }
+        public string NameSort { get; set; }
+        public string CurrentSort { get; set; }
+        public string searchFilter { get; set; }
 
         public SavedProducts(ILogger<SavedProducts> logger, ProductContext context)
         {
@@ -20,9 +24,40 @@ namespace WishList.Pages
             _logger = logger;
         }
 
-        public void OnGet()
+        public async Task OnGetAsync(string sortOrder, string currentFilter,
+            string searchString, int? pageIndex)
         {
-            SavedProductsList = _context.ProductMeta.ToList();    
+            CurrentSort = sortOrder;
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name" : "name_desc";
+
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            searchFilter = searchString;
+
+            IQueryable<ProductMeta> productMetas = from p in _context.ProductMeta select p;
+
+            if (!String.IsNullOrEmpty(searchFilter))
+            {
+                productMetas = productMetas.Where(p => p.NameHtmlId.Contains(searchFilter));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    productMetas = productMetas.OrderByDescending(p => p.NameHtmlId);
+                    break;
+                case "name":
+                    productMetas = productMetas.OrderBy(p => p.NameHtmlId);
+                    break;
+            }
+            int pageSize = 10;
+            SavedProductsList = await Paginate<ProductMeta>.CreateAsync(productMetas.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
         public void OnPost()
         {
