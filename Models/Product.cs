c5a81075065825f41f5b5b150da3d61d1d4c3003
Walker Chesley <chesley.walker@gmail.com>
2020-04-23 21:24:06 -0500

    added data annotations

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,12 +1,20 @@
-using System; 
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WishList
 {
     public class Product
     {
         public int Id {get; set;}
+        [DataType(DataType.DateTime)]
+        [Required]
         public DateTime timeRetreived {get; set;}
+        [Required]
+        [MinLength(1)]
         public string price {get; set;}
+        [Required]
+        [MinLength(1)]
         public string name {get; set;}
         public ProductMeta productMeta {get; set;}
     }
--- Models/ProductMeta.cs
+++ Models/ProductMeta.cs
@@ -1,13 +1,21 @@
 using System; 
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WishList
 {
     public class ProductMeta
     {
         public int Id {get; set;}
+        [DataType(DataType.Url)]
+        [Required]
         public string ProductUrl {get; set;}
+        [MinLength(1)]
+        [Required]
         public string PriceHtmlId {get; set;}
+        [MinLength(1)]
+        [Required]
         public string NameHtmlId {get; set;}
         public List<Product> products {get; set;} = new List<Product>();
     }
