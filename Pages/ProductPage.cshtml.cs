5b3dd4f12ab9a1eba5bd511f1cb9444e1682b2c9
Walker Chesley <chesley.walker@gmail.com>
2020-04-23 21:27:47 -0500

    added graph
    Took forever but I finally got it working; graph of all scraped prices with timestamp.

--- Pages/ProductPage.cshtml.cs
+++ Pages/ProductPage.cshtml.cs
@@ -6,34 +6,112 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using Newtonsoft.Json;
 
 namespace WishList
 {
     public class ProductPage : PageModel
     {
         private readonly ILogger<ProductPage> _logger;
-        private readonly ProductContext _context; 
-        public ProductMeta ProductDetails {get; set;} 
+        private readonly ProductContext _context;
+        [BindProperty]
+        public ProductMeta ProductDetails { get; set; }
+        public List<Product> ProductsList { get; set; }
+        public string chartXAxis { get; set; }
+        public string chartYAxis { get; set; }
+        [BindProperty]
+        [Required]
+        [DataType(DataType.Url)]
+        public string productUrl { get; set; }
+        [BindProperty]
+        [Required]
+        public string nameHtmlId { get; set; }
+        [BindProperty]
+        [Required]
+        public string priceHtmlId { get; set; }
         public ProductPage(ILogger<ProductPage> logger, ProductContext context)
         {
-            _context = context; 
-            _logger = logger; 
+            _context = context;
+            _logger = logger;
         }
+
+        /*
+        HTTP GET:
+        */
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
-            if(id == null)
+            if (id == null)
             {
-                return NotFound(); 
+                return NotFound();
+            }
+            _logger.LogInformation($"OnGET Fired:\n\tRequested id?:{id.ToString()}\n\tTime:{DateTime.Now.ToString()}");
+            ProductDetails = await _context.ProductMeta.Include(p => p.products)
+                .AsNoTracking().FirstOrDefaultAsync(pd => pd.Id == id);
+            if (ProductDetails == null)
+            {
+                return NotFound();
             }
 
-            ProductDetails = await _context.ProductMeta.Include( p => p.products)
-                .AsNoTracking().FirstOrDefaultAsync(pd => pd.Id == id);
-            if(ProductDetails == null)
+            ProductsList = _context.Product.Where(p => ProductDetails.Id == p.productMeta.Id).ToList();
+
+            List<Array> initX = new List<Array>();
+            List<double> innitY = new List<double>();
+            foreach (var product in ProductsList)
             {
-                return NotFound(); 
+                var tempY =  double.Parse(product.price, NumberStyles.AllowCurrencySymbol | NumberStyles.Number);
+                Array tempX = new object[] {product.timeRetreived.ToString()};
+                initX.Add(tempX);
+                innitY.Add(tempY);
             }
-            return Page(); 
+            var Xaxis = initX.ToList();
+            var Yaxis = innitY.ToList();
+            chartXAxis = JsonConvert.SerializeObject(Xaxis);
+            chartYAxis = JsonConvert.SerializeObject(Yaxis);
+            return Page();
+        }
+
+        /*
+        HTTP POST:
+        */
+
+        public async Task<IActionResult> OnPostDelete(int? id, int? parentId)
+        {
+            _logger.LogInformation("OnPostDelete Called...");
+            if (id == null || parentId == null)
+            {
+                return NotFound();
+            }
+            ProductDetails = await _context.ProductMeta.Include(p => p.products)
+                .AsNoTracking().FirstOrDefaultAsync(pd => pd.Id == parentId);
+            Product itemToDelete = await _context.Product.FindAsync(id);
+            try
+            {
+                _context.Product.Remove(itemToDelete);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./ProductPage?id=" + ProductDetails.Id);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error deleting obj");
+                return RedirectToPage("./ProductPage?id=" + ProductDetails.Id);
+            }
+        }
+        public async Task<IActionResult> OnPostUpdate(int? id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            ProductDetails = await _context.ProductMeta.FirstOrDefaultAsync(pd => pd.Id == id);
+            ProductDetails.NameHtmlId = nameHtmlId;
+            ProductDetails.PriceHtmlId = priceHtmlId;
+            ProductDetails.ProductUrl = productUrl;
+            await _context.SaveChangesAsync();
+            return RedirectToPage("./SavedProducts");
         }
     }
 }
