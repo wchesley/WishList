bdacf6d02cf864c8c7a951663c20da0f5bb705f1
Walker Chesley <chesley.walker@gmail.com>
2020-04-27 10:14:13 -0500

    Revert "formatting fix"
    This reverts commit ed2da7db5eecd092bae150014cb0504d3ef8789d.

--- Pages/ProductPage.cshtml.cs
+++ Pages/ProductPage.cshtml.cs
@@ -32,7 +32,7 @@ namespace WishList
         [Required]
         public string priceHtmlId { get; set; }
         [BindProperty]
-        public string vanityName { get; set; }
+        public string vanityName {get; set;}
 
         public ProductPage(ILogger<ProductPage> logger, ProductContext context)
         {
@@ -51,18 +51,41 @@ namespace WishList
                 return NotFound();
             }
             _logger.LogInformation($"OnGET Fired:\n\tRequested id?:{id.ToString()}\n\tTime:{DateTime.Now.ToString()}");
-
+            
             //Load 1:M relationship from DB: 
             ProductDetails = await _context.ProductMeta.Include(p => p.products)
                 .AsNoTracking().FirstOrDefaultAsync(pd => pd.Id == id);
-
+                
             if (ProductDetails == null)
             {
                 return NotFound();
             }
 
             ProductsList = ProductDetails.products.ToList();
-            PoputlateGraph(); 
+
+            //convert data to JSON for Chart: 
+            //prep two lists for x and y data: 
+            List<Array> initX = new List<Array>();
+            List<double> innitY = new List<double>();
+            foreach (var product in ProductsList)
+            {
+                double tempY = 0.00;
+                Array tempX = new object[] {product.timeRetreived.ToString()};
+                if(!product.price.Contains("Error"))
+                {
+                    //Y value needs to be a number: 
+                    //can ignore the dollar sign: https://docs.microsoft.com/en-us/dotnet/api/system.double.parse?redirectedfrom=MSDN&view=netframework-4.8#System_Double_Parse_System_String_System_Globalization_NumberStyles_ 
+                    tempY =  double.Parse(product.price, NumberStyles.AllowCurrencySymbol | NumberStyles.Number);
+                    initX.Add(tempX);
+                    innitY.Add(tempY); 
+                }                
+            }
+            //new list for storage: 
+            var Xaxis = initX.ToList();
+            var Yaxis = innitY.ToList();
+            //serialize into JSON and store in a string: 
+            chartXAxis = JsonConvert.SerializeObject(Xaxis);
+            chartYAxis = JsonConvert.SerializeObject(Yaxis);
             return Page();
         }
 
@@ -70,40 +93,26 @@ namespace WishList
         HTTP POST:
         */
 
-        public async Task<IActionResult> OnPostDelete(int? parentId, int? id)
+        public async Task<IActionResult> OnPostDelete(int? id, int? parentId)
         {
-            _logger.LogInformation($"OnPostDelete Called...\n");
-            if (id == null)
+            _logger.LogInformation("OnPostDelete Called...");
+            if (id == null || parentId == null)
             {
-                _logger.LogError("ID is null");
                 return NotFound();
             }
-            
-            //Page was throwing error due to graph not being populated: 
-            PoputlateGraph();
-            //var thing =  _context.Product.Where(p => p.Id == id).Include(p => p.productMeta).First(); 
             ProductDetails = await _context.ProductMeta.Include(p => p.products)
                 .AsNoTracking().FirstOrDefaultAsync(pd => pd.Id == parentId);
-            //ProductDetails = thing.productMeta;  
-            Product itemToDelete = ProductDetails.products.Where(p => p.Id == id).First();
-            // if (!ModelState.IsValid)
-            // {
-            //     _logger.LogError("Model State invalid!");
-            //     return Page();
-            // }
-            
-            
+            Product itemToDelete = await _context.Product.FindAsync(id);
             try
             {
                 _context.Product.Remove(itemToDelete);
                 await _context.SaveChangesAsync();
-                PoputlateGraph(); 
-                return RedirectToPage("SavedProducts");
+                return RedirectToPage("./ProductPage?id=" + ProductDetails.Id);
             }
             catch (Exception e)
             {
                 _logger.LogError(e, "Error deleting obj");
-                return RedirectToPage("./ProductPage?id=" + ProductDetails.Id.ToString());
+                return RedirectToPage("./ProductPage?id=" + ProductDetails.Id);
             }
         }
         public async Task<IActionResult> OnPostUpdate(int? id)
@@ -117,42 +126,9 @@ namespace WishList
             ProductDetails.NameHtmlId = nameHtmlId;
             ProductDetails.PriceHtmlId = priceHtmlId;
             ProductDetails.ProductUrl = productUrl;
-            ProductDetails.VanityName = vanityName;
+            ProductDetails.VanityName = vanityName; 
             await _context.SaveChangesAsync();
             return RedirectToPage("./SavedProducts");
-        }
-
-        public void PoputlateGraph()
-        {
-            if (ProductsList == null)
-            {
-                chartXAxis = JsonConvert.SerializeObject("No information");
-                chartYAxis = JsonConvert.SerializeObject("0");
-                return;
-            }
-            //convert data to JSON for Chart: 
-            //prep two lists for x and y data: 
-            List<Array> initX = new List<Array>();
-            List<double> innitY = new List<double>();
-            foreach (var product in ProductsList)
-            {
-                double tempY = 0.00;
-                Array tempX = new object[] { product.timeRetreived.ToString() };
-                if (!product.price.Contains("Error"))
-                {
-                    //Y value needs to be a number: 
-                    //can ignore the dollar sign: https://docs.microsoft.com/en-us/dotnet/api/system.double.parse?redirectedfrom=MSDN&view=netframework-4.8#System_Double_Parse_System_String_System_Globalization_NumberStyles_ 
-                    tempY = double.Parse(product.price, NumberStyles.AllowCurrencySymbol | NumberStyles.Number);
-                    initX.Add(tempX);
-                    innitY.Add(tempY);
-                }
-            }
-            //new list for storage: 
-            var Xaxis = initX.ToList();
-            var Yaxis = innitY.ToList();
-            //serialize into JSON and store in a string: 
-            chartXAxis = JsonConvert.SerializeObject(Xaxis);
-            chartYAxis = JsonConvert.SerializeObject(Yaxis);
         }
     }
 }
