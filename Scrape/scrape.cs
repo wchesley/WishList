964f42a20bc5e2d853d3dc8ab29a2a21aa8d5e6d
Walker Chesley <chesley.walker@gmail.com>
2020-04-29 01:21:47 -0500

    maybe this time

--- Scrape/scrape.cs
+++ Scrape/scrape.cs
@@ -21,7 +21,7 @@ namespace WishList
         {
             var options = new ChromeOptions();
             options.AddArguments("whitelisted-ips=''", "headless");
-            ChromeDriver browser = new ChromeDriver(@"./app/.chromedriver", options);
+            ChromeDriver browser = new ChromeDriver(".", options);
             //give pages 30 seconds to respond before timeing out: 
             browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
             return browser;
