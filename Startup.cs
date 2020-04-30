7825d5949d607fb2cd4a5ee34b85f7eb79124f00
Walker Chesley <chesley.walker@gmail.com>
2020-04-30 15:04:55 -0500

    perm fix for heroku conn string?
    THIS!!!!! https://github.com/nbarbettini/little-aspnetcore-book/pull/49/files

--- Startup.cs
+++ Startup.cs
@@ -15,7 +15,7 @@ using Npgsql;
 namespace WishList
 {
     public class Startup
-    {   
+    {
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -28,22 +28,40 @@ namespace WishList
         {
 
             // var test = Environment.GetEnvironmentVariable("DATABASE_URL", EnvironmentVariableTarget.Process);
-            // var databaseURI = new Uri(test);
-            // var userInfo = databaseURI.UserInfo.Split(':');
-            // var builder = new NpgsqlConnectionStringBuilder
-            // {
-            //     Host = databaseURI.Host,
-            //     Port = databaseURI.Port,
-            //     Username = userInfo[0],
-            //     Password = userInfo[1],
-            //     Database = databaseURI.LocalPath.TrimStart('/')
-            // };
-            // string psqlConnString = builder.ToString(); 
-            services.AddDbContext<ProductContext>(options =>
-                options.UseNpgsql(Configuration.GetConnectionString("ProductContext")));
+            services.AddEntityFrameworkNpgsql().AddDbContext<ProductContext>(options =>
+            {
+                var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+                string connStr;
+                // Depending on if in development or production, use either Heroku-provided
+                // connection string, or development connection string from env var.
+                if (env == "Development")
+                {
+                    // Use connection string from file.
+                    connStr = Configuration.GetConnectionString("LocalPSQL");
+                }
+                else
+                {
+                    // Use connection string provided at runtime by Heroku.
+                    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL", EnvironmentVariableTarget.Process);
+                    // Parse connection URL to connection string for Npgsql
+                    connUrl = connUrl.Replace("postgres://", string.Empty);
+                    var pgUserPass = connUrl.Split("@")[0];
+                    var pgHostPortDb = connUrl.Split("@")[1];
+                    var pgHostPort = pgHostPortDb.Split("/")[0];
+                    var pgDb = pgHostPortDb.Split("/")[1];
+                    var pgUser = pgUserPass.Split(":")[0];
+                    var pgPass = pgUserPass.Split(":")[1];
+                    var pgHost = pgHostPort.Split(":")[0];
+                    var pgPort = pgHostPort.Split(":")[1];
+                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}";
+                }
+                // Whether the connection string came from the local development configuration file
+                // or from the environment variable from Heroku, use it to set up your DbContext.
+                options.UseNpgsql(connStr);
+            });
             JobScheduler.Start();
             services.AddRazorPages();
-            
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
